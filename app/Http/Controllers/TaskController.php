8e28f1d4b7d73fe2d02c65387cfdfe17fdd17447
Emils <emils@gmail.com>
2025-06-06 10:14:07 +0300

    end of the class. done the task model and the controller

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,8 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Post;
-use Illuminate\Console\View\Components\Task;
+use App\Models\Task;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controllers\HasMiddleware;
 use Illuminate\Routing\Controllers\Middleware;
@@ -14,78 +13,39 @@ class PostController extends Controller implements HasMiddleware
     public static function middleware()
     {
         return [
-            new Middleware('auth:sanctum', except: ['index', 'show'])
+            new Middleware('auth:sanctum', except: ['index', 'show']),
         ];
     }
 
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         return Task::all();
     }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         $fields = $request->validate([
-            "title" = 'required '
-            "discription" = 'required '
-            "due_date" = 'required '
-            "status_id" = 'required '
-            "catagorya_id" = 'required '
+            "title" => 'required',
+            "description" => 'required',
+            "due_date" => 'required',
+            "status_id" => 'required',
+            "category_id" => 'required',
         ]);
 
-        $post =  $request->user()->posts()->create([
-            'title' => $request->input('title'),
-            'discription' => $request->input('discription')
-            'due_date' => $request->input('due_date'),
-            'status_id' => $request->input('status_id')
-            'catagorya_id' => $request->input('catagorya_id'),
-        ]);
-        $post->status()->updateOrCreate(
-            ['post_id' => $post->id],
-            ['status' => $request->input('status')] // or 'private'
+        $task = $request->user()->tasks()->create($fields);
+
+        $task->status()->updateOrCreate(
+            ['post_id' => $task->id],
+            ['status' => $request->input('status')]
         );
 
-        return $post;
+        return $task;
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Post $post)
+    public function show(Task $task)
     {
-        $post = Post::with('status')->find($post->id);
-        return $post;
+        return Task::with('status')->find($task->id);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Post $post)
+    public function update(Request $request, Task $task)
     {
-        Gate::authorize('change', $post);
-        $fields = $request->validate([
-            'title' => 'required|max:255',
-            'body' => 'required'
-        ]);
-
-        $post->update($fields);
-
-        return $post;
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Post $post)
-    {
-        Gate::authorize('change', $post);
-        $post->delete();
-        return ['message' => "The post ($post->id) has been deleted"];
-    }
-}
+        Gate::authorize('change', $
